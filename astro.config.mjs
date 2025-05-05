c1861a65cffda3237d84419d4cc630f6e637566b
tcheiner <tcheiner@gmail.com>
2025-05-06 00:53:43 +0200

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,17 @@
-<<<<<<< HEAD
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
-=======
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
-import sitemap from "@astrojs/sitemap";
+// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  // Add your domain here
-   site: 'https://lexingtonthemes.com',
-  integrations: [
-      sitemap(),
-      react()]
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    // Add your domain here
+    site: 'https://lexingtonthemes.com',
+    integrations: [
+        sitemap(),
+        react(),
+    ],
 });
->>>>>>> 1694624 (Initial commit)
