b668a6b4cc34208c5945cab9881bcc6457081ba8
tcheiner <tcheiner@gmail.com>
2025-05-28 10:29:17 +0200

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,21 @@
 import {defineConfig} from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dragonandbao.netlify.app/',
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    // Add your domain here
+    site: 'https://dragonandbao.netlify.app',
     integrations: [
+        sitemap(),
         react(),
         mdx(),
     ],
-    output: "server",
-    adapter: node({
-        mode: "standalone"
-    }),
+
 });
